74855e20db1f07ef88a6e124186c8d00615189df
Guptan0506 <navyag0509@gmail.com>
2024-09-23 11:40:32 -0400

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String name = "";
+        int age = 0;
+        double salary = 0.0;
+
+        System.out.println("What's your name?");
+        name = in.nextLine();
+        System.out.println("Your name is: "+ name);
+
+        System.out.println("How old are you?");
+        age=in.nextInt();
+        System.out.println("Your age is: "+ age);
+
+        System.out.println("What is your salary?");
+        salary = in.nextDouble();
+        System.out.println("Your salary is: "+ salary);
     }
 }
